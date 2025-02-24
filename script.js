ef3dc7aa6384f9759de604dab867c7171e3da49d
RitikNirwan99 <ritiknirwan99@gmail.com>
2025-02-24 18:24:13 +0530

    adding DOM Project 1

---
+++ script.js
@@ -0,0 +1,11 @@
+// Fetching the div element
+let div = document.getElementById("myDiv");
+        
+// Applying styles
+div.style.backgroundColor = "pink";
+div.style.margin = "20px";
+div.style.padding = "10px";
+div.style.fontSize = "18px";
+div.style.fontWeight = "bold";
+div.style.height = "200px";
+div.style.width = "300px";
